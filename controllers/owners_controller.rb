416963cc95468c785b97018dcaf2ffb9779573c8
Rory MacDonald <rorymacdonald6@hotmail.com>
2016-12-18 19:58:00 +0000

    Added owner views -edit

--- controllers/owners_controller.rb
+++ controllers/owners_controller.rb
@@ -1,8 +1,40 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
+require( 'pry' )
 require_relative( '../models/owner.rb' )
 
 get '/owners' do
   @owners = Owner.all()
   erb ( :"owners/index" )
+end
+
+post '/owners' do
+  @owner = Owner.new( params )
+  @owner.save()
+  erb(:"owners/create")
+end
+
+get '/owners/new' do
+  erb(:"owners/new")
+end
+
+get '/owners/:id' do
+  id = params[:id]
+  @owner = Owner.find(id)
+  erb(:"owners/show")
+end
+
+get '/owners/:id/edit' do
+  @owner = Owner.find(params[:id])
+  erb(:"owners/edit")
+end
+
+post '/pizzas/:id' do
+  Owner.update(params)
+  redirect to "owners/#{ params[:id] }"
+end
+
+post '/pizzas/:id/delete' do
+  Owner.destroy( params[:id] )
+  redirect to('/owners')
 end
